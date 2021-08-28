e66447d14108bd2e0c36ec42fe5d21d08085503f
Andriy Panchenko <72575014+SwiftXLight@users.noreply.github.com>
2021-08-28 15:31:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ module.exports = function towelSort(matrix) {
     return [];
   }
 
+  let tempArr = [];
+
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
-      let columnIdx = i % 2 === 0 ? j : (matrix[i].length - j - 1);
-      return (matrix[i][columnIdx]);
+      let columnIdx = i % 2 === 0 ? j : (matrix[i].length - 1 - j);
+      tempArr.push(matrix[i][columnIdx]);
     }
   }
+
+  return tempArr;
 }
